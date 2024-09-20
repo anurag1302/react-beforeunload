a706354a52fbf448aecb5cd680a428040ad3167b
anurag1302 <anurag3487@gmail.com>
2024-09-20 18:06:28 +0530

    use of beforeunload event in react

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [hasChanged, setHasChanged] = useState(false);
+
+  useEffect(() => {
+    function handleBeforeUnload(e) {
+      e.preventDefault();
+      e.returnValue = "";
+      return e.returnValue;
+    }
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [hasChanged]);
+
+  function handleOnChange() {
+    setHasChanged(true);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitted");
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1>Hello World!!</h1>
+        <form onChange={handleOnChange}>
+          <label>First Name</label>
+          <input type="text" name="firstName" />
+          <label>Last Name</label>
+          <input type="text" name="lastName" />
+          <button className="btn" type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
